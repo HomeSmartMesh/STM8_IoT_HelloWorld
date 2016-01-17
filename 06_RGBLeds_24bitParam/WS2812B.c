bd4f7de93b97daf5ae0d60deb4b6351d34b58269
wassfila <wassim.filali@gmail.com>
2016-01-17 19:13:18 +0100

    Updated the RGB Leds with color interpolation functions
    Useful to control a series of leds like 60 in this updated example,
    where only start and end colors are set. Also colors can easily be
    interpolated for colors animation sequences.

--- 06_RGBLeds_24bitParam/WS2812B.c
+++ 06_RGBLeds_24bitParam/WS2812B.c
@@ -20,8 +20,24 @@
 
 #include "ClockUartLed.h"
 
-unsigned char LedsArray[NbLeds];
-unsigned int nbLedsBytes = NbLeds*3;
+unsigned char LedsArray[NB_LEDS];
+unsigned int nbLedsBytes = NB_LEDS*3;
+
+
+const RGBColor_t RED = {255,0,0};
+const RGBColor_t GREEN = {0,255,0};
+const RGBColor_t BLUE = {0,0,255};
+
+
+//RGBLedPIN has to be defined by the user 0x02 for PIN_A2, 0x03 for PIN_A3
+
+#if(RGBLedPIN_A == 2)
+#define RGBLedPin_Set   "BSET    L:0x5000,      #0x02         \n"
+#define RGBLedPin_ReSet   "BRES    L:0x5000,      #0x02         \n"
+#else
+#define RGBLedPin_Set   "BSET    L:0x5000,      #0x03         \n"
+#define RGBLedPin_ReSet   "BRES    L:0x5000,      #0x03         \n"
+#endif
 
 void SendLedsArray()
 {
@@ -35,7 +51,7 @@ void SendLedsArray()
         "INCW    X                           \n"// L t+2
         "lb_start_bit0:                      \n"//
         "LD      A,           (L:LedsArray,X)\n"//
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set
         "AND     A,             #0x80        \n"// H t2
         "JREQ    L:lb_bit0_Send_0            \n"// H0 t3 t4 : 2 jump to Zero, 1 Stay in One + next nop
         "lb_bit0_Send_1:                     \n"//------Send 1 : 800ns High, 450ns Low (12,8) 750ns,500ns
@@ -47,13 +63,13 @@ void SendLedsArray()
         "nop                                 \n"// H1 t10
         "nop                                 \n"// H1 t11
         "nop                                 \n"// H1 t12
-        "BRES    L:0x5000,      #0x2         \n"// L1 t1
+        RGBLedPin_ReSet                         // L1 t1
         "nop                                 \n"// L1 t2
         "JRA     L:lb_start_bit1             \n"// L1 JRA:2 t4 
                                                 // L1 NextBitTest:4  t8
         "lb_bit0_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (7,13) 375ns,875ns
                                                 // H0 t4
-        "BRES    L:0x5000,      #0x2         \n"// L0 t1
+        RGBLedPin_ReSet                         // L0 t1
         "nop                                 \n"// L0 t1
         "nop                                 \n"// L0 t2
         "nop                                 \n"// L0 t3
@@ -70,7 +86,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit1_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit1_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -82,18 +98,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit2             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit1_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -111,7 +127,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit2_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit2_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -123,18 +139,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit3             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit2_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -152,7 +168,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit3_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit3_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -164,18 +180,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit4             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit3_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -193,7 +209,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit4_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit4_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -205,18 +221,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit5             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit4_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -234,7 +250,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit5_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit5_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -246,18 +262,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit6             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit5_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -275,7 +291,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit6_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit6_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -287,18 +303,18 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t2
         "JRA     L:lb_start_bit7             \n"// L JRA:2 t4 
                                                 // L NextBitTest:4  t8
         "lb_bit6_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -316,7 +332,7 @@ void SendLedsArray()
         "JREQ    L:lb_bit7_Send_0            \n"//2 jump to Zero, 1 Stay in One + next nop
         "lb_bit7_Send_1:                     \n"
         "nop                                 \n"//1 to have send0 send1 equality
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
@@ -328,19 +344,19 @@ void SendLedsArray()
         "nop                                 \n"// H t10
         "nop                                 \n"// H t11
         "nop                                 \n"// H t12
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "DECW    Y                           \n"//2
         "JREQ    L:lb_exit                   \n"//1 on loop, 2 jmp to exit
         "JP      L:lb_begin_loop             \n"//5
                                                 // 
         "lb_bit7_Send_0:                     \n"//------Send 0 : 400ns High, 850ns Low (6,14) 375ns,875ns
-        "BSET    L:0x5000,      #0x2         \n"// H t1
+        RGBLedPin_Set                           // H t1
         "nop                                 \n"// H t2
         "nop                                 \n"// H t3
         "nop                                 \n"// H t4
         "nop                                 \n"// H t5
         "nop                                 \n"// H t6
-        "BRES    L:0x5000,      #0x2         \n"// L t1
+        RGBLedPin_ReSet                         // L t1
         "nop                                 \n"// L t1
         "nop                                 \n"// L t2
         "nop                                 \n"// L t3
@@ -365,11 +381,34 @@ void SetLedColor(BYTE LedId, BYTE R, BYTE G, BYTE B)
   LedsArray[LedId*3+2] = B;
 }
 
+void SetLedColors(BYTE LedId,RGBColor_t Color)
+{
+  LedsArray[LedId*3] = Color.G;
+  LedsArray[LedId*3+1] = Color.R;
+  LedsArray[LedId*3+2] = Color.B;
+}
+
 void RGBLeds_PIO_Init_A2()
 {
 	PA_DDR_bit.DDR2 = 1;//output
 
 	PA_CR1_bit.C12 = 0;//0:open Drain - 1:PushPull
+}
+
+void RGBLeds_PIO_Init_A3()
+{
+	PA_DDR_bit.DDR3 = 1;//output
+
+	PA_CR1_bit.C13 = 0;//0:open Drain - 1:PushPull
+}
+
+void RGBLeds_PIO_Init()
+{
+  #if(RGBLedPIN_A == 2)
+  RGBLeds_PIO_Init_A2();
+  #else
+  RGBLeds_PIO_Init_A3();
+  #endif
 }
 
 //useful when only one Led is beeing used
@@ -383,6 +422,15 @@ void SendLedColor(BYTE R, BYTE G, BYTE B)
   delay_100us();//minimal delay for reset to restart the same color is 50 us
 }
 
+void RGB_SwitchOff(BYTE first,BYTE NbLeds)
+{
+	for(BYTE i=first;i<NbLeds;i++)
+	{
+		SetLedColor(i,0,0,0);
+	}
+	SendLedsArray();
+	delay_100us();//minimal delay for reset to restart the same color is 50 us
+}
 //delay : x255 meas 10 => 2.55 sec for cycle completion
 //XUp : 0 = unmodified, 1 = Up, 2 = Down
 //RGB : used when XUp = 0; unmodified stays at any user provided value
@@ -452,3 +500,44 @@ void BlinkColors(BYTE R, BYTE G, BYTE B)
   RampColors(delay,2&R,2&G,2&B,0,0,0);//Selected Colors Down
 }
 
+//input : two colors, and ratio integers
+//output : the interpolaetd color
+//Sets the interpolated color between Colors Start and End, to the ratio of iCount/nbCount
+RGBColor_t ColorScale(int iCount,int nbCount,RGBColor_t ColorStart,RGBColor_t ColorEnd)
+{
+    RGBColor_t resColor;
+    
+    int Red = (ColorEnd.R - ColorStart.R);//255
+    Red = Red * iCount;//0 - 2550
+    Red = ColorStart.R + (Red / (nbCount - 1));//0 - 255
+    resColor.R = Red;
+
+    int Green = (ColorEnd.G - ColorStart.G);//255
+    Green = Green * iCount;//0 - 2550
+    Green = ColorStart.G + (Green / (nbCount - 1));//0 - 255
+    resColor.G = Green;
+
+    int Blue = (ColorEnd.B - ColorStart.B);//255
+    Blue = Blue * iCount;//0 - 2550
+    Blue = ColorStart.B + (Blue / (nbCount - 1));//0 - 255
+    resColor.B = Blue;
+
+    return resColor;
+}
+
+//inputs: the leds section to use defined by which led to start with and which one to end before
+//inputs: The color of the first led to use and the color of the last led to use
+//action: updates the leds memory with the first and last led colors and all intermediate leds are interpolated
+//comment: This function does not send the led data bits on the bus, so that multiple operations can 
+//         be applied before sending the whole result together with SendLedsArray();
+void ShadeLeds(BYTE LedStart, BYTE LedEnd, RGBColor_t ColorStart, RGBColor_t ColorEnd)
+{
+  int nbLeds = LedEnd - LedStart;
+  BYTE LedId = LedStart;
+  for(int iCount=0;iCount<nbLeds;iCount++)//0-10
+  {
+    RGBColor_t Ci = ColorScale(iCount,nbLeds,ColorStart,ColorEnd);
+    SetLedColors(LedId,Ci);
+    LedId++;
+  }
+}
